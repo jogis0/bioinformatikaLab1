c19f5693ea64428119be87e720c9f1ddc1e68534
Jokubas Viliusis <viliusisj@gmail.com>
2024-10-13 16:13:20 +0300

    Kodonu, dikodonu daznis ir atsumas

--- lab1.py
+++ lab1.py
@@ -1,5 +1,8 @@
 from Bio import SeqIO
 from collections import Counter
+import os
+import math
+
 
 # splice one of the 6 sequences into subsequences with start and stop codons
 def get_valid_sequences(sequence):
@@ -37,7 +40,7 @@ def translate_sequence(sequences):
     return protein_sequences
 
 
-def get_codon_frequency(sequences):
+def get_codon_count(sequences):
     combined_codons = ""
     for seq in sequences:
         for s in seq:
@@ -46,7 +49,7 @@ def get_codon_frequency(sequences):
     return Counter(combined_codons)
 
 
-def get_dicodon_frequency(sequences):
+def get_dicodon_count(sequences):
     dicodon_count = Counter()
 
     for seq in sequences:
@@ -77,18 +80,55 @@ def get_protein_sequences(sequence):
     return protein_sequences
 
 
-file = "C:\\Users\\viliu\\PycharmProjects\\bioinformatikaLab1\\data\\bacterial1.fasta"
-it = SeqIO.parse(file, "fasta")
+def get_frequency_from_counter(counter):
+    freq = {}
+    for val in counter:
+        if not val.endswith('*'):
+            freq[val] = counter[val] / sum(counter.values())
+    return freq
 
-for record in it:
-    protein_sequences = get_protein_sequences(record.seq)
-    codon_counter = get_codon_frequency(protein_sequences)
-    dicodon_counter = get_dicodon_frequency(protein_sequences)
 
-    print("Codon counter\n")
-    print(codon_counter)
-    print("Dicodon counter\n")
-    print(dicodon_counter)
-    # surasti daznius
-    # pagal daznius apskaiciuoti atstuma pagal random kazkokia funkcija
-    # atstumai yra tarp failu
+def get_frequency_from_file(filename):
+    path = os.path.abspath(os.curdir) + os.path.sep + "data" + os.path.sep + filename
+    seq_frequencies = ()
+    it = SeqIO.parse(path, "fasta")
+
+    for record in it:
+        protein_sequences = get_protein_sequences(record.seq)
+        codon_counter = get_codon_count(protein_sequences)
+        dicodon_counter = get_dicodon_count(protein_sequences)
+
+        codon_freq = get_frequency_from_counter(codon_counter)
+        dicodon_freq = get_frequency_from_counter(dicodon_counter)
+
+        seq_frequencies = (codon_freq, dicodon_freq)
+
+    return seq_frequencies
+
+
+def calculate_key_distance(freq1, freq2):
+    all_keys = set(freq1.keys()).union(set(freq2.keys()))
+    distance = 0.0
+
+    for key in all_keys:
+        freq1 = freq1.get(key, 0.0)
+        freq2 = freq2.get(key, 0.0)
+
+        distance += (freq1 - freq2) ** 2
+
+    return math.sqrt(distance)
+
+
+def calculate_distance(sequence1_freq, sequence2_freq):
+    codon_distance = calculate_key_distance(sequence1_freq[0], sequence2_freq[0])
+    dicodon_distance = calculate_key_distance(sequence1_freq[1], sequence2_freq[1])
+
+    return (codon_distance + dicodon_distance) / 2
+
+
+bacterial1_freq = get_frequency_from_file("bacterial1.fasta")
+bacterial2_freq = get_frequency_from_file("bacterial2.fasta")
+print(calculate_distance(bacterial1_freq, bacterial2_freq))
+# surasti daznius ✓
+# pagal daznius apskaiciuoti atstuma pagal random kazkokia funkcija [ Distance(A, B) sqrt(sum(i=1, n)(Ai - Bi)^2) ] ✓
+# atstumai yra tarp failu
